e2a8c403ab61b34eb2c5ac9771c7d0029e6b1903
Viktor Borza <viktorborza@rarecrew.com>
2017-07-04 11:21:50 +0200

    tests added

---
+++ ConsoleApp/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(2,2);
+        }
+    }
+}
